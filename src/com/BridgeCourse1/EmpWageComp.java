08088ece70414d00b68ecfb290c01b50abdd602b
Soumyadip Majumder <somdip94@gmail.com>
2021-02-09 02:03:13 +0530

    src/com/BridgeCourse1/EmpWageComp.java

---
+++ src/com/BridgeCourse1/EmpWageComp.java
@@ -0,0 +1,10 @@
+package com.BridgeCourse1;
+
+public class EmpWageComp {
+	
+	public static void main(String args []) {
+		
+		System.out.println("Hello Welcome to emp wage calculation program");
+	}
+
+}
