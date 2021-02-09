019eedce44d0ac10a633a0697042265a425152d7
Soumyadip Majumder <somdip94@gmail.com>
2021-02-09 17:37:25 +0530

    adding hello message

--- src/com/BridgeCourse1/EmpWageComp.java
+++ src/com/BridgeCourse1/EmpWageComp.java
@@ -2,9 +2,12 @@ package com.BridgeCourse1;
 
 public class EmpWageComp {
 	
-	public static void main(String args []) {
+	public static void main(String args [])
+	{
 		
 		System.out.println("Hello Welcome to emp wage calculation program");
+		
+		
 	}
 
 }
---
+++ src/com/BridgeCourse1/LineComparison.java
@@ -0,0 +1,14 @@
+package com.BridgeCourse1;
+
+public class LineComparison {
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println(" welcome to the line Comparison Program");
+		
+		
+		
+	}
+
+}
